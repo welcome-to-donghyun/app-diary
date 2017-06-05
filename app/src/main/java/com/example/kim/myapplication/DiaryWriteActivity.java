1002eaa05cad99711eb35b60cac6de05cf8d60ec
skaska121212 <skaska121212@gmail.com>
2017-06-05 22:37:01 +0900

    Activity들 추가

---
+++ app/src/main/java/com/example/kim/myapplication/DiaryWriteActivity.java
@@ -0,0 +1,13 @@
+package com.example.kim.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiaryWriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary_write);
+    }
+}
